ce8cea5acb2d71c0e08ff5072a25d41a6b3c2e0a
Artsiom <artsiombarodka@gmail.com>
2020-03-03 21:20:02 +0300

    create AppConfiguration class; change db package: - add Connection$proxy class - change ConnectionPoolImpl class

--- src/main/java/epam/my/project/db/impl/ConnectionPoolImpl.java
+++ src/main/java/epam/my/project/db/impl/ConnectionPoolImpl.java
@@ -1,24 +1,43 @@
 package epam.my.project.db.impl;
 
+import static epam.my.project.config.ApplicationConfiguration.*;
+
+import epam.my.project.config.ApplicationConfiguration;
 import epam.my.project.db.ConnectionPool;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public enum  ConnectionPoolImpl implements ConnectionPool {
     CONNECTION_POOL_INSTANCE;
 
-    private List<Connection> availableConnections = new ArrayList<>();
+    private List<Connection> availableConnections = new CopyOnWriteArrayList<Connection>(new ArrayList<Connection>(CONFIGURATION_INSTANCE.getDbInitialPoolSize()));
     private List<Connection> takenConnections = new ArrayList<>();
 
     ConnectionPoolImpl(){
-
+        initConnections();
     }
 
 
     public Connection getConnection() {
-        return null;
+        Connection connection = null;
+
+        if(!availableConnections.isEmpty()){
+            connection = availableConnections.get(0);
+
+            if(Objects.nonNull(connection)){
+                takenConnections.add(connection);
+                availableConnections.remove(0);
+            }
+
+        }
+
+        return connection;
     }
 
     public boolean releaseConnection(Connection connection) {
@@ -27,5 +46,25 @@ public enum  ConnectionPoolImpl implements ConnectionPool {
 
     public void shutdown() {
 
+    }
+
+    private void initConnections(){
+        createConnections(CONFIGURATION_INSTANCE.getDbInitialPoolSize());
+        //log
+    }
+
+    private void createConnections(int valueOfConnections){
+        for (int i = 0; i < valueOfConnections; i++) {
+            try {
+                Connection connection = new Connection$Proxy(DriverManager.getConnection(
+                        CONFIGURATION_INSTANCE.getDbUrl(),
+                        CONFIGURATION_INSTANCE.getDbUser(),
+                        CONFIGURATION_INSTANCE.getDbPassword()));
+
+                availableConnections.add(connection);
+            } catch (SQLException e) {
+                //log
+            }
+        }
     }
 }
